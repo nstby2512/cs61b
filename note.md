97f7fff6e33713acb915bdc368d8845ffffc44a8
nstby2512 <143398921+nstby2512@users.noreply.github.com>
2024-12-09 11:43:04 +0800

    note和HelloWorld、WelloNumber

---
+++ note.md
@@ -0,0 +1,5 @@
+# cs61b
+
+## Lab 1
+
+- Our print statement is just `System.out.print` instead of `System.out.println`. This means we should not include a newline (a return)
---
+++ week1/HelloNumber.java
@@ -0,0 +1,14 @@
+package cs61b;
+
+public class HelloNumber {
+    public static void main(String[] args){
+        int x = 0;
+        int y = 0;
+        while (x < 10){
+            System.out.print(y + " ");
+            x = x + 1;
+            y = y + x;
+        }
+    }
+    
+}
